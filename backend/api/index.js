7f5e0122aefb06dffd8651f0254ad353751aec5a
KartikAg13 <23803013@mail.jiit.ac.in>
2025-02-21 22:35:42 +0530

    fix space

--- backend/api/index.js
+++ backend/api/index.js
@@ -4,22 +4,22 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const options = {
-  method: 'GET',
-  url: `https://${process.env.RAPIDAPI_HOST}/api/v1/hotels/searchDestination`,
-  params: { query: 'man' },
-  headers: {
-    'x-rapidapi-key': process.env.RAPIDAPI_KEY,
-    'x-rapidapi-host': process.env.RAPIDAPI_HOST
-  }
+    method: 'GET',
+    url: `https://${process.env.RAPIDAPI_HOST}/api/v1/hotels/searchDestination`,
+    params: { query: 'man' },
+    headers: {
+        'x-rapidapi-key': process.env.RAPIDAPI_KEY,
+        'x-rapidapi-host': process.env.RAPIDAPI_HOST
+    }
 };
 
 async function fetchData() {
-  try {
-    const response = await axios.request(options);
-    console.log(response.data);
-  } catch (error) {
-    console.error("API Error:", error.response ? error.response.data : error.message);
-  }
+    try {
+        const response = await axios.request(options);
+        console.log(response.data);
+    } catch (error) {
+        console.error("API Error:", error.response ? error.response.data : error.message);
+    }
 }
 
 fetchData();
