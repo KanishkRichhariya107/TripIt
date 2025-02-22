b58f1541573549caa2618b21eba4e278c609d4f0
KartikAg13 <23803013@mail.jiit.ac.in>
2025-02-22 19:16:10 +0530

    ocd

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import styles from "./index.module.css"
 // import './main.css'
-// import App from './App.jsx'
-import Home from './Home.jsx'
+import App from './App.jsx'
+// import Home from './Home.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Home />
+    <App />
   </StrictMode>,
 )
